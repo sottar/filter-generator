bfbbc11cd1a2109d9243889647a6c6b86b4f2fe3
sottar <ohrst.18@gmail.com>
2016-03-15 10:13:02 +0900

    #1 add autoprefix and delete prefixes in sass files

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,7 +3,8 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-typescript');
-  grunt.registerTask("default", ["connect", "watch", "typescript", "compass"]);
+  grunt.loadNpmTasks('grunt-autoprefixer');
+  grunt.registerTask("default", ["connect", "watch", "typescript", "compass", "autoprefixer"]);
   grunt.initConfig({
     connect: {
       livereload: {
@@ -30,12 +31,20 @@ module.exports = function(grunt) {
         files: ['src/stylesheets/style.css']
       },
       scss: {
-        files: ['src/sass/style.scss'],
-        tasks: ['compass']
+        files: ['src/sass/*.scss'],
+        tasks: ['compass', 'autoprefixer']
       },
       ts: {
         files: ['src/typescript/*.ts'],
         tasks: ['typescript']
+      }
+    },
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version', 'ie 8']
+      },
+      dist: {
+        src: ['src/stylesheets/*.css'],
       }
     }
   });
